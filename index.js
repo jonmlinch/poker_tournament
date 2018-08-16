55034f134b2e942ca72af53ab9324df694ddfd7c
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-15 18:40:09 -0700

    class code along making express app to add or delete team names

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //Requires
+var bodyParser = require("body-parser");
 var express = require("express");
 var ejsLayouts = require("express-ejs-layouts");
+var teamsDB = require('./models/teamservice.js');
 
 //Global vars
 var app = express();
@@ -8,10 +10,47 @@ var app = express();
 //Set and Use statements
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
 
 //Routes
 app.get("/", function(req, res){
   res.render("home");
+});
+
+app.get("/teams", function(req, res){
+	var allTeams = teamsDB.getAllTeams();
+	res.render("teams/index", {teams: allTeams});
+});
+
+app.get("/teams/new", function(req, res){
+	res.render("teams/new");
+})
+
+app.get("/teams/:name", function(req, res){
+	var team = teamsDB.getOneTeam(req.params.name);
+	res.render("teams/show", {team: team})
+});
+
+app.post("/teams", function(req, res){
+	teamsDB.addTeam(req.body);
+	res.redirect("/teams/" + req.body.name);
+
+})
+
+app.get("/teams/edit/:name", function(req, res){
+	var team = teamsDB.getOneTeam(req.params.name);
+	res.render("teams/edit", {team: team})
+});
+
+app.put("/teams/:name", function(req, res){
+	teamsDB.editTeam(req.params.name, req.body)
+	res.send("PUUUUUT")
+});
+
+app.delete("/teams/:name", function(req, res){
+	teamsDB.deleteTeam(req.params.name);
+	res.send("Delete!")
 });
 
 //Listen
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
   console.log('Hello from teams-ajax.js!');
   // TO DO: Define calls to PUT and DELETE routes here
-});
+  $("#edit-form").submit(function(e){
+  	e.preventDefault();
+  	var teamUrl = $(this).attr("action");
+  	var teamData = $(this).serialize();
+  	console.log('url is', teamUrl)
+  	console.log('data', teamData)
+
+  	$.ajax({
+  		method: "PUT",
+  		url: teamUrl,
+  		data: teamData,
+  	}).done(function(data){
+  		console.log("success!", data);
+  		window.location = teamUrl;
+  	}).fail(function(err){
+  		console.log("error", err)
+  	}); //end of ajax
+  }) //end of edit-form submit
+
+  $("#delete-btn").click(function(e){
+  	e.preventDefault();
+  	var teamUrl = $(this).attr("href");
+  	console.log("stuff is working", teamUrl)
+  	$.ajax({
+  		method: "DELETE",
+  		url: teamUrl,
+  	}).done(function(data){
+  		console.log('success', data);
+  		window.location = "/teams";
+  	}).fail(function(err){
+  		console.log("err", err);
+  	})
+  })
+}); //end of document ready
